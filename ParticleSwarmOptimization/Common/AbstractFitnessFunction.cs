a3b9146080abf9d436a45a14694967d7118b143c
rosiakh <hubert.rosiak@gmail.com>
2016-01-10 19:37:18 +0100

    Added abstract fitness function class

---
+++ ParticleSwarmOptimization/Common/AbstractFitnessFunction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Common
+{
+    public abstract class AbstractFitnessFunction
+    {
+        public abstract double Calculate(double[] vector);
+    }
+
+    public class QuadraticFunction : AbstractFitnessFunction
+    {
+        public int Dimension;
+        public double[] Coefficients;
+
+        public override double Calculate(double[] vector)
+        {
+            double value = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                value += Coefficients[i] * vector[i];
+            }
+            return value;
+        }
+    }
+
+    public class RastriginFunction : AbstractFitnessFunction
+    {
+        public int Dimension;
+        public double[] Coefficients;
+
+        public override double Calculate(double[] vector)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class RosenbrockFunction : AbstractFitnessFunction
+    {
+        public int Dimension;
+        public double[] Coefficients;
+
+        public override double Calculate(double[] vector)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
