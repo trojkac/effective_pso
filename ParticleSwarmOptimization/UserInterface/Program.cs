fffbb12332ee0fdeb73d79b9a50ac4601f15872e
Kacper Trojanowski <trojanowski.kacper@gmail.com>
2016-12-19 20:46:15 +0100

    add gpu params to UI app

--- ParticleSwarmOptimization/UserInterface/Program.cs
+++ ParticleSwarmOptimization/UserInterface/Program.cs
@@ -1,50 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Xml.Serialization;
 using Common;
 using Common.Parameters;
 using Node;
 
 namespace UserInterface
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+
+
+      var nodeParamsDeserialize = new ParametersSerializer<NodeParameters>();
+      var nodeParams = nodeParamsDeserialize.Deserialize("nodeParams.xml");
+      var machineManager = new MachineManager(nodeParams.Ip, nodeParams.Ports.ToArray());
+
+      var cont = true;
+      while (cont)
+      {
+        PrintMenu();
+        var c = Console.ReadKey().KeyChar;
+        Console.WriteLine("\n\n");
+
+        switch (c)
         {
-
-            var nodeParamsDeserialize = new ParametersSerializer<NodeParameters>();
-            var psoParamsDeserialize = new ParametersSerializer<PsoParameters>();
-            var nodeParams = nodeParamsDeserialize.Deserialize("nodeParams.xml");
-            var psoParams = psoParamsDeserialize.Deserialize("psoParams.xml");
-            
-            var machineManager = new MachineManager(nodeParams.Ip,nodeParams.Ports.ToArray());
-
-            var cont = true;
-            while (cont)
-            {
-                Console.WriteLine("1 - Start Calculations");
-                Console.WriteLine("0 - Exit");
-                Console.WriteLine("");
-                Console.Write("choice:");
-                var c = Console.ReadKey().KeyChar;
-                Console.WriteLine("\n\n");
-
-                switch (c)
-                {
-                    case '1':
-                        machineManager.StartPsoAlgorithm(psoParams);
-                        var r = machineManager.GetResult();
-                        Console.WriteLine("Value: {0}",r.FitnessValue[0]);
-                        break;
-                    case '0':
-                        cont = false;
-                        break;
-                }
-            }
+          case '1':
+            var r = PerformCalculations(machineManager);
+            Console.WriteLine("Value: {0}", r.FitnessValue[0]);
+            break;
+          case '0':
+            cont = false;
+            break;
         }
+      }
     }
+
+    private static void PrintMenu()
+    {
+      Console.WriteLine("1 - Start Calculations");
+      Console.WriteLine("0 - Exit");
+      Console.WriteLine("");
+      Console.Write("choice:");
+
+    }
+
+    private static ParticleState PerformCalculations(MachineManager machineManager)
+    {
+      var psoParamsDeserialize = new ParametersSerializer<PsoParameters>();
+      var psoParams = psoParamsDeserialize.Deserialize("psoParams.xml");
+
+      machineManager.StartPsoAlgorithm(psoParams);
+      return machineManager.GetResult();
+    }
+  }
 }
